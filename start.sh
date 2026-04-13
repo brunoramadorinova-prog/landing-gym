1b62448fa454e9b54b70ce113c48da840d76d922
Bruno Ramadori <bruno.ramadori.nova@gmail.com>
2026-04-13 17:28:07 -0300

    Dejando PORT a Railway

--- start.sh
+++ start.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-pip install gunicorn flask flask-cors psycopg2-binary bcrypt google-auth requests mercadopago pyjwt
-exec gunicorn main:app --bind 0.0.0.0:$PORT --workers 1
+exec gunicorn main:app
